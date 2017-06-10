926f7396091e078fe9946c67044708225afa1b90
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-10 19:00:29 +0200

    Explicitliy added the headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Headers, Http} from '@angular/http';
 
 @Injectable()
 export class ServerService {
   constructor(private http: Http) {}
+
   storeServers(servers: any[]) {
-    return this.http.post('https://ng-http-a127b.firebaseio.com/data.json', servers);
+    const headers = new Headers({'Content-Type': 'application.json'});
+    return this.http.post('https://ng-http-a127b.firebaseio.com/data.json', servers, {headers: headers});
   }
 }
